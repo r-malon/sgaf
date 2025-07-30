0bd2a5d03546b95f27c8acbfc4aac12f8de21701
usuario <33752131+r-malon@users.noreply.github.com>
2025-07-30 16:32:17 -0300

    Small fixes

--- handleLocal.go
+++ handleLocal.go
@@ -7,19 +7,14 @@ import (
 	"github.com/r-malon/sgaf/db"
 )
 
+func createLocal(w http.ResponseWriter, r *http.Request) error {
+	return q.CreateLocal(ctx, r.FormValue("nome"))
+}
+
 func listLocals(w http.ResponseWriter, r *http.Request) error {
 	l, err := q.ListLocals(ctx)
 	tmpl.ExecuteTemplate(w, "listLocals", l)
 	return err
-}
-
-func deleteLocal(w http.ResponseWriter, r *http.Request) error {
-	id, _ := strconv.Atoi(r.PathValue("id"))
-	return q.DeleteLocal(ctx, int64(id))
-}
-
-func createLocal(w http.ResponseWriter, r *http.Request) error {
-	return q.CreateLocal(ctx, r.FormValue("nome"))
 }
 
 func updateLocal(w http.ResponseWriter, r *http.Request) error {
@@ -27,4 +22,9 @@ func updateLocal(w http.ResponseWriter, r *http.Request) error {
 	nome := r.FormValue("nome")
 	data := db.UpdateLocalParams{nome, int64(id)}
 	return q.UpdateLocal(ctx, data)
+}
+
+func deleteLocal(w http.ResponseWriter, r *http.Request) error {
+	id, _ := strconv.Atoi(r.PathValue("id"))
+	return q.DeleteLocal(ctx, int64(id))
 }
